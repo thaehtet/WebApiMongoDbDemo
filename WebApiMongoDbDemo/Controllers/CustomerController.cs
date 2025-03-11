ab97ce2fd25d961695e910eabae0b77b45cca576
sophia <sophia@visibleone.com>
2025-03-11 14:21:07 +0630

    add project file

---
+++ WebApiMongoDbDemo/Controllers/CustomerController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+using WebApiMongoDbDemo.Data;
+using WebApiMongoDbDemo.Entity;
+
+namespace WebApiMongoDbDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly IMongoCollection<Customer> _customers;
+        public CustomerController(MongoDbService mongoDbService)
+        {
+            _customers = mongoDbService.database?.GetCollection<Customer>("customer");
+
+        }
+        [HttpGet]
+        public async Task<IEnumerable<Customer>> Get()
+        {
+            return await _customers.Find(FilterDefinition<Customer>.Empty).ToListAsync();
+
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Customer?>> GetById(string id)
+        {
+            var filter = Builders<Customer>.Filter.Eq(x => x.Id, id);
+            var customer = await _customers.Find(filter).FirstOrDefaultAsync();
+            return customer is null ? Ok(customer) : NotFound();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create(Customer customer)
+        {
+            await _customers.InsertOneAsync(customer);
+            return CreatedAtAction(nameof(GetById), new { id = customer.Id }, customer);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> Update(Customer customer)
+        {
+            var filter = Builders<Customer>.Filter.Eq(x => x.Id,customer.Id);
+            //var update = Builders<Customer>.Update
+            //    .Set(x => x.CustomerName, customer.CustomerName)
+            //    .Set(x => x.Email, customer.Email);
+            //await _customers.UpdateOneAsync(filter, update);
+            await _customers.ReplaceOneAsync(filter, customer);
+            return Ok();
+        }
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(string id)
+        {
+            var filter = Builders<Customer>.Filter.Eq(x => x.Id, id);
+            await _customers.DeleteOneAsync(filter);
+            return Ok();
+        }
+    }
+}
---
+++ WebApiMongoDbDemo/Data/MongoDbService.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Driver;
+
+namespace WebApiMongoDbDemo.Data
+{
+    public class MongoDbService
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IMongoDatabase? _database;
+        public MongoDbService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            var connectionString = _configuration.GetConnectionString("DbConnection");
+            var mongoUrl = MongoUrl.Create(connectionString);
+            var mongoClient = new MongoClient(mongoUrl);
+            _database = mongoClient.GetDatabase(mongoUrl.DatabaseName);
+        }
+        public IMongoDatabase? database => _database;
+    }
+}
